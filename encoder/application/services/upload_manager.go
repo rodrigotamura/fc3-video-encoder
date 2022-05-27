c60e9c5eac02f2dfe6321d728b64b551e038a40e
Rodrigo Tamura <rodrigo.tamura@ifood.com.br>
2022-05-27 09:31:16 -0300

    feat: added video loader

--- encoder/application/services/upload_manager.go
+++ encoder/application/services/upload_manager.go
@@ -3,7 +3,9 @@ package services
 import (
 	"context"
 	"io"
+	"io/fs"
 	"os"
+	"path/filepath"
 	"strings"
 
 	"cloud.google.com/go/storage"
@@ -43,4 +45,36 @@ func (vu *VideoUpload) UploadObject(objectPath string, client *storage.Client, c
 
 	return nil
 
+}
+
+func (vu *VideoUpload) loadPaths() error {
+
+	err := filepath.Walk(vu.VideoPath, func(path string, info fs.FileInfo, err error) error {
+		// for each directory it will exec this func
+		if !info.IsDir() {
+			vu.Paths = append(vu.Paths, path)
+		}
+
+		return nil
+
+	})
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+
+}
+
+func getClientUpload() (*storage.Client, context.Context, error) {
+	ctx := context.Background()
+
+	client, err := storage.NewClient(ctx)
+
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return client, ctx, nil
 }
