c9e90d88128f67fcf0e1d5d1883531bcc53df143
Rodrigo Tamura <rodrigo.tamura@ifood.com.br>
2022-06-08 08:51:08 -0300

    feat: implementing jobs

---
+++ encoder/application/services/job_service.go
@@ -0,0 +1,42 @@
+package services
+
+import (
+	"encoder/application/repositories"
+	"encoder/domain"
+)
+
+type JobService struct {
+	Job           *domain.Job
+	JobRepository repositories.JobRepository
+	VideoService  VideoService
+}
+
+func (receiver *JobService) Start() error {
+
+}
+
+func (j JobService) changeJobStatus(status string) error {
+	var err error
+
+	j.Job.Status = status
+	j.Job, err = j.JobRepository.Update(j.Job)
+
+	if err != nil {
+		return j.failJob(err)
+	}
+
+	return nil
+}
+
+func (j JobService) failJob(error error) error {
+	j.Job.Status = "FAILED"
+	j.Job.Error = error.Error()
+
+	_, err := j.JobRepository.Update(j.Job)
+
+	if error != nil {
+		return err
+	}
+
+	return error
+}
